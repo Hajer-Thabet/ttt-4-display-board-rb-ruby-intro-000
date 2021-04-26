4935244acb0a3f30d48d5ff1cf642c3aa26d2abc
Hajer Thabet <hajerthabet10@gmail.com>
2021-04-26 17:39:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,9 +5,9 @@
 def display_board (bo)
   puts " #{bo[0]} | #{bo[1]} | #{bo[2]} "
   puts "-----------"
-  puts "#{bo[3]}|#{bo[4]}|#{bo[5]}"
+  puts " #{bo[3]} | #{bo[4]} | #{bo[5]} "
   puts "-----------"
-  puts "#{bo[6]}|#{bo[7]}|#{bo[8]}"
+  puts " #{bo[6]} | #{bo[7]} | #{bo[8]} "
 
 end
 board= [" ", " ", " ", " ", " ", " ", " ", " ", " "]
